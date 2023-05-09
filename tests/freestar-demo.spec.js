db6f1a99efd207fe63c17b2d2801abc9edf58bbf
Greg-test99 <gregory.oliver99@gmail.com>
2023-05-09 14:35:25 +0100

    add test to capture console logged errors

---
+++ tests/freestar-demo.spec.js
@@ -0,0 +1,33 @@
+// @ts-check
+const { test } = require("@playwright/test");
+const fs = require("fs");
+
+const now = new Date();
+const folderName = `error-reports-${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
+const fileName = `errors-${Date.now()}.txt`;
+
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+} catch (err) {
+  console.error(err);
+}
+
+const writeLogs = function (msg) {
+  fs.appendFile(`${folderName}/${fileName}`, `${msg.text()}\r\n`, (err) => {
+    if (err) console.log(err);
+  });
+};
+
+test("Extract console logs to file", async ({ page }) => {
+  page.on("console", (msg) => {
+    if (msg.type() === "error") {
+      writeLogs(msg);
+    }
+  });
+  await page.goto("https://demos.freestar.com/products/display");
+  await page.getByRole("button", { name: "AGREE" }).click();
+  //TODO: how to fix this hard wait - when does the last error happen?
+  await page.waitForTimeout(10000);
+});
